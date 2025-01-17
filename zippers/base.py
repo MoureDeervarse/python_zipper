a44354da00b86df291848113720b12922e8351dd
MoureDeervarse <moure.deervarse@gmail.com>
2025-01-18 07:10:51 +1100

    missed change added

--- zippers/base.py
+++ zippers/base.py
@@ -2,9 +2,9 @@ from abc import *
 
 class Zipper(metaclass=ABCMeta):
     @abstractmethod
-    def compress(self, target_path, output_file):
+    def compress(self, target_path: str, output_file: str):
         pass
 
     @abstractmethod
-    def decompress(self, target_file, output_file):
+    def decompress(self, target_file: str, output_file: str):
         pass
